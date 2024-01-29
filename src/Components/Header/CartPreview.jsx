1b806ab8a14f514c1129cd992f8dfe8b094bddaa
Emmett Herrick <123272376+Rinnemme@users.noreply.github.com>
2024-01-29 15:47:48 -0500

    Set up basic styling and functionality for a cart preview built off of cartItems from the context provided by App

--- src/Components/Header/CartPreview.jsx
+++ src/Components/Header/CartPreview.jsx
@@ -1,9 +1,45 @@
 // A small window with a scrollable list of cart items, and buttons to go to cart or checkotu
 
 import CartLogo from '../../assets/cart.png'
+import { useState, useContext } from 'react'
+import { StoreContext } from '../App.jsx'
+import { Link } from 'react-router-dom'
 
 export default function CartPreview() {
+    const context = useContext(StoreContext)
+    const cartItems = context.cartItems
+    const [CartVisible, setCartVisible] = useState(false)
+    let cartTotal = 0
+    cartItems.forEach(item => cartTotal = cartTotal+(item.price * item.quantity))
     return (
-        <img src={CartLogo}/>
+        <>
+            <img src={CartLogo} onClick={() => setCartVisible(true)}/>
+            {CartVisible && <div id="cart-container" onMouseLeave={() => setCartVisible(false)}>
+                <div id="cart-preview">
+                    <div id="mini-cart-banner">In Your Cart:</div>
+                    <div id="mini-cart">
+                        {cartItems.length===0 && <div><em>There are no items in your cart.</em></div>}
+                        {cartItems.map(item=>{
+                            return (
+                                <div class="mini-cart-item">
+                                    <img src={item.thumbnail}/>
+                                    <div class="mini-cart-item-name">{`${item.title} (${item.quantity})`}</div>
+                                    <div class="mini-cart-item-total">{`$${item.price * item.quantity}`}</div>
+                                </div>
+                            )
+                        })}
+                    </div>
+                    <div id="mini-cart-total">
+                        <div>Total:</div>
+                        <div>{`$${cartTotal}`}</div>
+                    </div>
+                    <div id="mini-cart-buttons">
+                        <Link to='/cart'><button id="left-mini-cart-button">To Cart</button></Link>
+                        <Link to='/checkout'><button id="right-mini-cart-button">To Checkout</button></Link>
+                    </div>
+                </div>
+            </div>
+            }
+        </>
     )
 }
