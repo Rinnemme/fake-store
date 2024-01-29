8ba4a1ec343ec03886b7d403a86997321d809972
Emmett Herrick <123272376+Rinnemme@users.noreply.github.com>
2024-01-29 15:51:35 -0500

    split Header.css into three css files - one for header itself, and one for the two components rendered within header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,6 +2,8 @@
 // hamburger and cart should, when clicked, render a list of links & a cart preview respectively
 
 import './Header.css'
+import './HamburgerMenu.css'
+import './CartPreview.css'
 import HamburgerMenu from './HamburgerMenu.jsx'
 import CartPreview from './CartPreview.jsx'
 import Logo from '../../assets/logo.svg'
