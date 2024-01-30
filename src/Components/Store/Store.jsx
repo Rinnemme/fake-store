f1ad8c0e460d7e63039c505d9eb0299090dc612b
Emmett Herrick <123272376+Rinnemme@users.noreply.github.com>
2024-01-30 10:54:46 -0500

    set up basic styling for store to render a heading for each category, and item cards for each category (only visuals, no interactivity)

--- src/Components/Store/Store.jsx
+++ src/Components/Store/Store.jsx
@@ -3,27 +3,63 @@
 import {useContext, useState} from 'react'
 import {StoreContext} from '../App.jsx'
 import Header from '../Header/Header.jsx'
+import './Store.css'
 
 function Store() {
     const context = useContext(StoreContext)
     const shopItems = context.shopItems
-    const cartItems = context.cartItems
-    const updateCart = context.updateCart
-
-    const [Count, setCount] = useState(0)
+    // const cartItems = context.cartItems
+    // const updateCart = context.updateCart
+    // const [Count, setCount] = useState(0)
+    let categories = []
+    shopItems.forEach(item => {
+        if(!categories.includes(item.category)) {
+            categories.push(item.category)
+        }
+    })
 
     return (
         <>
             <Header />
-            {cartItems.map(item => {
+            {/* {cartItems.map(item => {
                 return <div key={item.id}>{`${item.title}`}</div>
             })}
-            <div>lol</div>
             <button onClick={() => {
-                    cartItems.push(shopItems[Count])
+                    cartItems.push({...shopItems[Count], quantity:1})
                     setCount(Count+1)
                     updateCart([...cartItems])
-                }}>UpdateCart</button>
+                }}>Add thing to cart</button> */}
+            <div id="store-container">
+                <div id="store-sidebar"></div>
+                <div id="store-main">
+                    {categories.map(category => {
+                        const categoryItems = shopItems.filter(item=>item.category===category)
+                        return (
+                            <div className="store-section-container" key={`${category}`}>
+                                <div className="store-section-header">{`${category.toUpperCase()}`}</div>
+                                <div className="store-section">
+                                    {categoryItems.map(item => {
+                                        return (
+                                            <div key={item.id} className="store-item-card">
+                                                <div className="store-item-image" style={{backgroundImage:`url(${item.thumbnail})`}}/>
+                                                <div className="store-item-info">
+                                                    <h4>{`${item.title}`}</h4>
+                                                    <p>{`${item.description}`}</p>
+                                                    <div className="add-to-cart-section">
+                                                        <div>Quantity:</div>
+                                                        <input type="number" max="5" defaultValue="1"></input>
+                                                        <button>Add to Cart</button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        )
+                                    })}
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
         </>
     )
 }
