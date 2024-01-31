7aefa6407505c667bcb649fd3ccfebd9b41f892e
Emmett Herrick <123272376+Rinnemme@users.noreply.github.com>
2024-01-31 14:54:49 -0500

    make logo in header a Link to home

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -7,12 +7,13 @@ import './CartPreview.css'
 import HamburgerMenu from './HamburgerMenu.jsx'
 import CartPreview from './CartPreview.jsx'
 import Logo from '../../assets/logo.svg'
+import { Link } from 'react-router-dom'
 
 export default function Header() {
     return (
         <div id="store-header">
             <HamburgerMenu />
-            <img id="logo" src={Logo}/>
+            <Link to='/'><img id="logo" src={Logo}/></Link>
             <CartPreview />
         </div>
     )
