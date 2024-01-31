3a12363219f32c392ab4059382402b606f91947f
Emmett Herrick <123272376+Rinnemme@users.noreply.github.com>
2024-01-31 17:05:39 -0500

    flesh out error page and add contingencies if fetch fails in App.jsx

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from 'react'
 import Router from './Router.jsx'
 import { defaultCart } from './DefaultCart.jsx'
+import '../index.css'
 
 const StoreContext = createContext({
   cartItems: [],
@@ -13,7 +14,10 @@ const loadedCart = localStorage.getItem('cart') ? JSON.parse(localStorage.getIte
 function App() {
   const [CartItems, setCartItems] = useState(loadedCart)
   const [ShopItems, setShopItems] = useState([])
-
+  const [FetchFailed, setFetchFailed] = useState(false)
+  const [ErrorMessage, setErrorMessage] = useState('')
+  const [Loading, setLoading] = useState(true)
+ 
   function updateCart(cart) {
     setCartItems(cart)
     localStorage.setItem('cart', JSON.stringify(cart))
@@ -24,8 +28,12 @@ function App() {
       await fetch('https://dummyjson.com/products', {mode: 'cors'})
       .then(result=>result.json())
       .then(json=>setShopItems(json.products))
+      .finally(setLoading(false))
     }
-    catch (err) {console.log(err)}
+    catch (err) {
+      setErrorMessage(`${err}`)
+      setFetchFailed(true)
+    }
   }
 
   useEffect(() => {
@@ -34,13 +42,30 @@ function App() {
 
   return (
     <>
+      {FetchFailed &&  <>
+            <div id="modal">
+                <div id="modal-message">
+                    <p>{`Looks like we failed to get the data necessary here!`}</p>
+                    <p>{`(${ErrorMessage})`}</p>
+                    <button onClick={() => location.reload()}>Refresh</button>
+                </div>
+            </div>
+      </>}
+      {!FetchFailed && Loading && <>
+            <div id="modal">
+                <div id="modal-message">
+                    <p>{`Loading...`}</p>
+                </div>
+            </div>
+      </>}
+      {!FetchFailed && !Loading &&
       <StoreContext.Provider value = {{
         cartItems: CartItems,
         shopItems: ShopItems,
         updateCart: updateCart,
       }}>
         <Router />
-      </StoreContext.Provider>
+      </StoreContext.Provider>}
     </>
   )
 }
--- src/Components/ErrorPage.jsx
+++ src/Components/ErrorPage.jsx
@@ -1,5 +1,15 @@
+import '../index.css'
+import { Link } from 'react-router-dom'
+
 export default function Error() {
     return (
-        <div>Ruh roh, raggy</div>
+        <>
+            <div id="modal">
+                <div id="modal-message">
+                    <p>Whoops, looks like there's nothing here!</p>
+                    <Link to='/'><button>Go Home</button></Link>
+                </div>
+            </div>
+        </>
     )
 }
