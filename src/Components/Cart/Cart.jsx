7651369a0bb52b5a64ded5b6f018e89a51dc1af5
Emmett Herrick <123272376+Rinnemme@users.noreply.github.com>
2024-02-01 14:11:23 -0500

    set up flex-wrapping and additional containers to make each cart row wrap nicely for mobile, and ensure images don't stretch to goofy proportions

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -46,10 +46,12 @@ export default function Cart() {
                         {cartItems.map(item => {
                             return (
                                 <div key={item.id} className="cart-item-row">
-                                    <div className="cart-item-image" style={{backgroundImage:`url(${item.thumbnail})`}}/>
-                                    <div className="cart-item-name">{`${item.title}`}</div>
+                                        <div className="cart-item-image" style={{backgroundImage:`url(${item.thumbnail})`}}/> 
                                     <div className="cart-item-info">
-                                        <div className="cart-item-price">{`$${item.price}`}</div>
+                                        <div className="name-and-price">
+                                            <div className="cart-item-name">{`${item.title}`}</div>
+                                            <div className="cart-item-price">{`$${item.price}`}</div>
+                                        </div>
                                         <div className="cart-item-quantity-section">
                                             <div>Qty:</div>
                                             <input id={`${item.title}-cart-quantity`} type="number" max="5" min="1" defaultValue={item.quantity}></input>
