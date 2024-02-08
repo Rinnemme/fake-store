6467462b577e370056409f5eddabcd66271f9217
Emmett Herrick <123272376+Rinnemme@users.noreply.github.com>
2024-02-08 13:59:55 -0500

    Write out Sidebar component which takes a categories array prop so it doesn't require duplicate context code, using existinc code from store.jsx

---
+++ src/Components/Store/Sidebar.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react"
+import LeftArrow from '../../assets/left-arrow.svg'
+import RightArrow from '../../assets/right-arrow.svg'
+
+export default function Sidebar({categories}) {
+    const[ShowSidebar, setShowSidebar] = useState(true)
+
+    function capitalize(string) {
+        return string.slice(0,1).toUpperCase()+string.slice(1)
+    }
+
+    return (
+        <>
+            {!ShowSidebar && <img id="store-sidebar-open" src={RightArrow} onClick={() => setShowSidebar(true)}></img>}
+                {ShowSidebar && <div id="store-sidebar">
+                    <div id="store-sidebar-content">
+                        <img id="store-sidebar-close" src={LeftArrow} onClick={() => setShowSidebar(false)}></img>
+                        <h2>Categories</h2>
+                        {categories.map(category => {
+                            return (<h3 key={category}><a href={`#${category}`}>{`${capitalize(category)}`}</a></h3>)
+                        })}
+                    </div>
+            </div>}
+        </>
+    )
+}
