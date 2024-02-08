2cc5360051de4ea1722d084943b5cf392d907599
Emmett Herrick <123272376+Rinnemme@users.noreply.github.com>
2024-02-08 15:21:37 -0500

    change sidebar's state to use camelCase and fix second conditional rendering in return to not be indented unnecessarily

--- src/Components/Store/Sidebar.jsx
+++ src/Components/Store/Sidebar.jsx
@@ -3,7 +3,7 @@ import LeftArrow from '../../assets/left-arrow.svg'
 import RightArrow from '../../assets/right-arrow.svg'
 
 export default function Sidebar({categories}) {
-    const[ShowSidebar, setShowSidebar] = useState(true)
+    const[showSidebar, setShowSidebar] = useState(true)
 
     function capitalize(string) {
         return string.slice(0,1).toUpperCase()+string.slice(1)
@@ -11,15 +11,15 @@ export default function Sidebar({categories}) {
 
     return (
         <>
-            {!ShowSidebar && <img id="store-sidebar-open" src={RightArrow} onClick={() => setShowSidebar(true)}></img>}
-                {ShowSidebar && <div id="store-sidebar">
-                    <div id="store-sidebar-content">
-                        <img id="store-sidebar-close" src={LeftArrow} onClick={() => setShowSidebar(false)}></img>
-                        <h2>Categories</h2>
-                        {categories.map(category => {
-                            return (<h3 key={category}><a href={`#${category}`}>{`${capitalize(category)}`}</a></h3>)
-                        })}
-                    </div>
+            {!showSidebar && <img id="store-sidebar-open" src={RightArrow} onClick={() => setShowSidebar(true)}></img>}
+            {showSidebar && <div id="store-sidebar">
+                <div id="store-sidebar-content">
+                    <img id="store-sidebar-close" src={LeftArrow} onClick={() => setShowSidebar(false)}></img>
+                    <h2>Categories</h2>
+                    {categories.map(category => {
+                        return (<h3 key={category}><a href={`#${category}`}>{`${capitalize(category)}`}</a></h3>)
+                    })}
+                </div>
             </div>}
         </>
     )
