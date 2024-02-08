1f1d7633a091eaf6b8e29b1c7d91a1b281e11fb7
Emmett Herrick <123272376+Rinnemme@users.noreply.github.com>
2024-02-08 15:20:19 -0500

    change hamburgermenu's state to use camelCase and change its toggleMenuVisibility to a simple boolean

--- src/Components/Header/HamburgerMenu.jsx
+++ src/Components/Header/HamburgerMenu.jsx
@@ -3,17 +3,16 @@ import MenuLogo from '../../assets/hamburger.png'
 import { Link } from 'react-router-dom'
 
 export default function HamburgerMenu() {
-    const [MenuVisible, setMenuVisible] = useState(false)
+    const [menuVisible, setMenuVisible] = useState(false)
 
     function toggleMenuVisibility() {
-        if (MenuVisible === false) setMenuVisible(true)
-        else setMenuVisible(false)
+        setMenuVisible(!menuVisible)
     }
 
     return (
         <>
             <img src={MenuLogo} onClick={() => toggleMenuVisibility()}/>
-            {MenuVisible && <div id="menu-container" onMouseLeave={() => setMenuVisible(false)}>
+            {menuVisible && <div id="menu-container" onMouseLeave={() => setMenuVisible(false)}>
                     <div id="menu">
                         <Link to='/'>Home</Link>
                         <Link to='/store'>Store</Link>
